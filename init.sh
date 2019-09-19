36eed6d0609201973a05d490f36c09cee6f094a2
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-19 04:05:56 +0000

    init

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-python -m venv venv \
-&& source venv/bin/activate \
-&& python setup.py develop
-&& python punctuator/task/allennlp_tagger.py --embed 100 --hidden 100 --epoch 100
+python -m venv venv
+source venv/bin/activate  
+python setup.py develop 
+python punctuator/task/allennlp_tagger.py --embed 100 --hidden 100 --epoch 100
