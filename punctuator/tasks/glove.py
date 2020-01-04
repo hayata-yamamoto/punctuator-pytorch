73fd262b4d5bac3097df81d450f8e54e4d2b3a36
hayata-yamamoto <hayata.pri.yamamoto@gmail.com>
2020-01-04 14:52:33 +0900

    rename files and create glove.py

--- punctuator/tasks/glove.py
+++ punctuator/tasks/glove.py
@@ -19,7 +19,7 @@ from punctuator.src.datasets.datasets import (
     PunctuatorDatasetReader,
     PunctuatorTokenizer,
 )
-from punctuator.src.datasets.utils import reconstruct
+from punctuator.src.datasets.utils import reconstruct, replacing
 from punctuator.src.models import Punctuator
 from punctuator.src.path_manager import PathManager
 
@@ -32,21 +32,16 @@ def main():
     dev_dataset = reader.read(str(PathManager.RAW / "dev.csv"))
 
     vocab = Vocabulary.from_instances(train_dataset + dev_dataset)
-    token_embedding = Embedding(vocab.get_vocab_size(), Config.EMBED_DIM)
-    # token_embedding = Embedding.from_params(
-    #     vocab,
-    #     Params({
-    #         "embedding_dim": 200,
-    #         "pretrained_file": '(https://nlp.stanford.edu/data/glove.twitter.27B.zip)#glove.twitter.27B.200d.txt'
-    #     }))
+    token_embedding = Embedding.from_params(
+        vocab,
+        Params({
+            "embedding_dim": 200,
+            "pretrained_file": '(https://nlp.stanford.edu/data/glove.twitter.27B.zip)#glove.twitter.27B.200d.txt'
+        }))
     word_embeddings = BasicTextFieldEmbedder({"tokens": token_embedding})
 
-    gru = PytorchSeq2SeqWrapper(torch.nn.GRU(Config.EMBED_DIM, Config.HIDDEN_DIM, batch_first=True,
-                                             bidirectional=True))
-    print(gru.get_output_dim())
-    # gru = PytorchSeq2SeqWrapper(torch.nn.GRU(200, Config.HIDDEN_DIM, batch_first=True, bidirectional=True))
+    gru = PytorchSeq2SeqWrapper(torch.nn.GRU(200, Config.HIDDEN_DIM, batch_first=True, bidirectional=True))
     attn = IntraSentenceAttentionEncoder(input_dim=gru.get_output_dim(), combination='1')
-    print(attn.get_output_dim())
     model: Punctuator = Punctuator(word_embeddings, gru, vocab, attn)
 
     if torch.cuda.is_available():
@@ -66,8 +61,8 @@ def main():
         train_dataset=train_dataset,
         validation_dataset=dev_dataset,
         validation_metric="+accuracy",
-        patience=10,
-        summary_interval=10,
+        patience=2,
+        summary_interval=2,
         num_epochs=Config.EPOCH,
         cuda_device=cuda_device,
     )
@@ -93,22 +88,13 @@ def main():
 
     print(classification_report(true, pred))
 
-    s = """Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to perform a specific task without using explicit instructions relying on patterns and inference instead It is seen as a subset of artificial intelligence Machine learning algorithms build a mathematical model based on sample data known as "training data" in order to make predictions or decisions without being explicitly programmed to perform the task Machine learning algorithms are used in a wide variety of applications such as email filtering and computer vision where it is difficult or infeasible to develop a conventional algorithm for effectively performing the task"""
-    true = """Machine learning (ML) is the scientific study of algorithms and statistical models that computer systems use to perform a specific task without using explicit instructions, relying on patterns and inference instead. It is seen as a subset of artificial intelligence. Machine learning algorithms build a mathematical model based on sample data, known as "training data", in order to make predictions or decisions without being explicitly programmed to perform the task. Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision, where it is difficult or infeasible to develop a conventional algorithm for effectively performing the task."""
-
-    logit = predictor.predict(str(s))["tag_logits"]
-    idx = [np.argmax(logit[i], axis=-1) for i in range(len(logit))]
-    pred += [model.vocab.get_token_from_index(i, "labels") for i in idx]
-
-    print("==============")
-    print("True Sentence ")
-    print("==============")
-    print(true)
-
-    print("==============")
-    print("Predict Sentence ")
-    print("==============")
-    print(reconstruct(s.strip().split(" "), pred))
+    for s in tqdm(df.head()["0"]):
+        sent = replacing(str(s))
+        logit = predictor.predict(sent)["tag_logits"]
+        idx = [np.argmax(logit[i], axis=-1) for i in range(len(logit))]
+        print(s)
+        pred = [model.vocab.get_token_from_index(i, "labels") for i in idx]
+        print(reconstruct(s.strip().split(" "), pred))
 
 
 if __name__ == "__main__":
